23363e2a9d13119d6c59d6e43274ffa7bcc78d6f
AlexJohnMorenoInga <amorenoinga@gmail.com>
2024-02-10 18:59:03 -0500

    avance hasta video 8

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+//import React from 'react'; //<-- agregado por mi
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import './App.css';
+
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
+
+function App() {
+  return (
+    <Router>
+      <Navigation/>
+      
+      <Routes>
+        <Route path="/" exact Component={NotesList} />
+        <Route path="/edit/:id" Component={CreateNote} />
+        <Route path="/create" Component={CreateNote} />
+        <Route path="/user" Component={CreateUser} />
+      </Routes>
+
+    </Router>
+  );
+}
+
+export default App;
