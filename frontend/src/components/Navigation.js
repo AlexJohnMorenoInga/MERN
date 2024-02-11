8ba698924e11f13735ceaa901e8cc10a28b46d99
AlexJohnMorenoInga <amorenoinga@gmail.com>
2024-02-11 00:25:19 -0500

    avance hasta video 9

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Navigation extends Component {
   render() {
     return (
-      <div>
-        NAVIGATION
-      </div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              NotesApp
+            </Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <Link className="nav-link" to="/">Notes</Link>
+                </li>
+                <li class="nav-item">
+                    <Link className="nav-link" to="/create">Create Note</Link>
+                </li>
+                <li class="nav-item">
+                    <Link className="nav-link" to="/user">Create User</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
